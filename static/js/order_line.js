ae93d6b4595a1cca5d967af3329a4a580cf9e148
Tajinder Pal Singh Chahal <tajinder.chahal14@gmail.com>
2015-12-02 16:48:16 +0530

    Update order_line.js

--- static/js/order_line.js
+++ static/js/order_line.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
   console.log('This is hello from github');
+  var status_select = '<select>' +
+      '<option value="1">Pending</option>' +
+      '<option value="2">Processing</option>' +
+      '<option value="2">Cancelled</option>' +
+    '</select>'
+  $('.field-status').each(function(){
+    $(this).html(status_select);
+  })
 });
