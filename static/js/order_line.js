6a8f972e2f10b93044a4b5cbdf340e02e9744222
Tajinder Pal Singh Chahal <tajinder.chahal14@gmail.com>
2015-12-02 17:04:46 +0530

    Update order_line.js

--- static/js/order_line.js
+++ static/js/order_line.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
   console.log('This is hello from github');
-  var status_select = $('<select style="width: 130px;">' +
-      '<option value="1">Pending</option>' +
-      '<option value="2">Processing</option>' +
-      '<option value="2">Cancelled</option>' +
-    '</select>');
+  var order_status = ['Processing','Ready to Pick','Inbound QC','Packing','Ready To Ship',
+    'Shipped','Delivered','Closed','Cancelled','Return Requested','Return Denied',
+    'Return accepted','Exchange','Refund'];
+  var status_select = '<select data-selected_status="" style="width: 130px;">';
+  $.each(order_status, function(i, v){
+    status_select += '<option value="'+ v +'">'+ v +'</option>';
+  });
+  status_select += '</select>';
   $('.field-status').each(function(){
-    //$(this).html($(status_select));
+    var cur_status = $(this).html();
+    $(status_select).data('selected_status', cur_status);
+    $(this).html($(status_select));
   });
   
   
